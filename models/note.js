b766065e640a7116f48c19f7b38a56b839a3ce45
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-10 15:00:50 -0500

    add test

--- models/note.js
+++ models/note.js
@@ -1,34 +1,18 @@
 const mongoose = require('mongoose')
 
-// eslint-disable-next-line no-undef
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log(url)
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-// Create a schema to save an specify data
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   date: {
     type: Date,
-    required: true
+    required: true,
   },
   important: Boolean,
 })
 
-// Set a custom schema rsponse
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -37,5 +21,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-// Bind data with modal and export
 module.exports = mongoose.model('Note', noteSchema)
