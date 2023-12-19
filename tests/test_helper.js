be2c6363d7158bf1a3b3899aa921312a5bc4ffcd
Jonathan Tellez <jonathanbtellez@gmail.com>
2023-12-19 08:13:49 -0500

    refactor test

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -16,7 +16,7 @@ const initialNotes = [
 const nonExistingId = async () => {
   const note = new Note({ content: 'willremovethissoon', date: new Date() })
   await note.save()
-  await note.remove()
+  await Note.findByIdAndDelete(note._id)
 
   return note._id.toString()
 }
